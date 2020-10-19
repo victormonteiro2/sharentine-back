968d839b352b0d45f4ef8b5497a510622b07d621
Victor Monteiro <victor_monteiro@live.com>
2020-10-18 23:18:47 -0300

    code refactoring

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 require('dotenv').config();
-require('./configs/db.config');
-require('./configs/passport.config')(app);
 
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
-const authRouter = require('./routes/auth.routes');
-const postRouter = require('./routes/post.routes');
-const commentRouter = require('./routes/comment.routes');
-const path = require('path');
 
 // Configura o app para entender requisições com tipo de corpo JSON
 app.use(express.json());
 app.use(cors({ origin: process.env.CORS }));
 
+const authRouter = require('./routes/auth.routes');
+const postRouter = require('./routes/post.routes');
+const commentRouter = require('./routes/comment.routes');
+
+require('./configs/db.config');
+
+require('./configs/passport.config')(app);
+
 app.use('/api', authRouter);
 app.use('/api', postRouter);
 app.use('/api', commentRouter);
+
 app.use('/api', require('./routes/file-upload-routes'));
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(process.env.PORT, () =>
   console.log(`running at port ${process.env.PORT}`)
 );
