0c58512965da48df447d3acb1575752b635f867f
Victor Monteiro <victor_monteiro@live.com>
2020-10-19 00:58:33 -0300

    code refactoring

--- app.js
+++ app.js
@@ -2,36 +2,40 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 // Configura o app para entender requisições com tipo de corpo JSON
 app.use(express.json());
-app.use(cors({ origin: process.env.CORS }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: process.env.REACT_APP_URL }));
 
+const projectRouter = require('./routes/project.routes');
+const taskRouter = require('./routes/task.routes');
 const authRouter = require('./routes/auth.routes');
-const postRouter = require('./routes/post.routes');
-const commentRouter = require('./routes/comment.routes');
 
 require('./configs/db.config');
 
 require('./configs/passport.config')(app);
 
-app.use('/api', authRouter);
-app.use('/api', postRouter);
-app.use('/api', commentRouter);
+const publicPath = path.join(__dirname, 'public');
 
-app.use('/api', require('./routes/file-upload-routes'));
+app.use(express.static(publicPath));
+
+app.get('*', (req, res, next) => {
+  const hostUrl = req.originalUrl;
+  if (!hostUrl.includes('/api')) {
+    console.log(hostUrl);
+    return res.sendFile(path.join(publicPath, 'index.html'));
+  }
+  return next();
+});
+
+app.use('/api', projectRouter);
+app.use('/api', taskRouter);
+app.use('/api', authRouter);
 
 app.listen(process.env.PORT, () =>
   console.log(`running at port ${process.env.PORT}`)
 );
-
-app.use(express.static(path.join(__dirname, '/public')));
-app.use((req, res, next) => {
-  const hostUrl = req.get('host');
-  if (hostUrl.includes('/api') === true) {
-    return res.sendFile(__dirname + '/public/index.html');
-  }
-  return;
-});
