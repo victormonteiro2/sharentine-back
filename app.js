0cdd93f7e84a0c16c2ffd20ce4de7b4989c8c175
Victor Monteiro <victor_monteiro@live.com>
2020-10-19 02:19:51 -0300

    code refactoring

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 
 // Configura o app para entender requisições com tipo de corpo JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: process.env.REACT_APP_URL }));
+app.use(cors()); //{ origin: process.env.CORS }
 
 const authRouter = require('./routes/auth.routes');
 const postRouter = require('./routes/post.routes');
 const commentRouter = require('./routes/comment.routes');
+const path = require('path');
 
 require('./configs/db.config');
 
@@ -23,11 +22,17 @@ app.use('/api', authRouter);
 app.use('/api', postRouter);
 app.use('/api', commentRouter);
 
+app.use('/api', require('./routes/file-upload-routes'));
+
+app.listen(process.env.PORT, () =>
+  console.log(`running at port ${process.env.PORT}`)
+);
+
 const publicPath = path.join(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res, next) => {
+app.get('*', (req, res, next) => {
   const hostUrl = req.originalUrl;
   if (!hostUrl.includes('/api')) {
     console.log(hostUrl);
@@ -36,6 +41,11 @@ app.get('/', (req, res, next) => {
   return next();
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`running at port ${process.env.PORT}`)
-);
+app.use(express.static(path.join(__dirname, '/public')));
+app.use((req, res, next) => {
+  const hostUrl = req.get('host');
+  if (hostUrl.includes('/api') === true) {
+    return res.sendFile(__dirname + '/public/index.html');
+  }
+  return;
+});
