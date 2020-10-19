b72f4839e44a24cc424cc75846c0f788a183d7ab
Victor Monteiro <victor_monteiro@live.com>
2020-10-18 23:00:19 -0300

    build done

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 require('dotenv').config();
+require('./configs/db.config');
+require('./configs/passport.config')(app);
 
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
-
-// Configura o app para entender requisições com tipo de corpo JSON
-app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
-
 const authRouter = require('./routes/auth.routes');
 const postRouter = require('./routes/post.routes');
 const commentRouter = require('./routes/comment.routes');
+const path = require('path');
 
-require('./configs/db.config');
-
-require('./configs/passport.config')(app);
+// Configura o app para entender requisições com tipo de corpo JSON
+app.use(express.json());
+app.use(cors({ origin: process.env.CORS }));
 
 app.use('/api', authRouter);
 app.use('/api', postRouter);
 app.use('/api', commentRouter);
-
 app.use('/api', require('./routes/file-upload-routes'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(process.env.PORT, () =>
+  console.log(`running at port ${process.env.PORT}`)
+);
 
-app.listen(5000, () => console.log('running at port 5000'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use((req, res, next) => {
+  const hostUrl = req.get('host');
+  if (hostUrl.includes('/api') === true) {
+    return res.sendFile(__dirname + '/public/index.html');
+  }
+  return;
+});
---
+++ public/precache-manifest.4d7a5aaf632834ae296090147b00be64.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13c7b63322eca09ea39354cd742d7d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f0c61c83d1941d01a0",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9208f9c821f8bb2d74f7",
+    "url": "/static/css/main.e4d1e57f.chunk.css"
+  },
+  {
+    "revision": "78f0c61c83d1941d01a0",
+    "url": "/static/js/2.b573567b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b573567b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9208f9c821f8bb2d74f7",
+    "url": "/static/js/main.68365d88.chunk.js"
+  },
+  {
+    "revision": "dec01eeaffa8065aebd3",
+    "url": "/static/js/runtime-main.7075ca4d.js"
+  },
+  {
+    "revision": "0398b0a5825046354cf87da11a14e3d0",
+    "url": "/static/media/MovieImg.0398b0a5.svg"
+  },
+  {
+    "revision": "9262e830cf35b770db75d49aa4c7b946",
+    "url": "/static/media/MusicImg.9262e830.svg"
+  },
+  {
+    "revision": "125cf05c2b16b4965db690e3ad008dd5",
+    "url": "/static/media/camila.125cf05c.png"
+  },
+  {
+    "revision": "9a85d9c58b6720de390dd5205979be33",
+    "url": "/static/media/howimg.9a85d9c5.svg"
+  },
+  {
+    "revision": "e5c4566de5d342bf1fa7eebf0a6717af",
+    "url": "/static/media/initialimg.e5c4566d.svg"
+  },
+  {
+    "revision": "e97754e26b36db078007fb107ee5ec9b",
+    "url": "/static/media/share.e97754e2.svg"
+  },
+  {
+    "revision": "cd21d399b5ee3784ca12ad28da2a9075",
+    "url": "/static/media/undraw_book_lover_mkck.cd21d399.svg"
+  }
+]);
